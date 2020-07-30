eb896fa6061d5b685e7e4a990b308a24b1766103
Ashkan Ebtekari <56440241+AshkanE11@users.noreply.github.com>
2020-07-30 15:10:18 +0430

    HelloWorld For My Diary ...

---
+++ HelloWorld/src/com/company/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("\n Hey Chipmunk !!! \n Im Using GitHub");
+
+
+    }
+
+}
